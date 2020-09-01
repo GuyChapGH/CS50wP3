c614645400e37445bf8e80e8bf3452b864f1c600
Guy Chaplin <guy.chaplin@googlemail.com>
2020-09-01 14:42:17 +0100

    Completed Send Mail function started Mailbox function

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -43,17 +43,10 @@ function compose_email() {
           console.log(result);
       });
 
-      // Load user's SENT mailbox HERE. This becomes: load_mailbox('sent');
-      // and the fetch moves into load_mailbox function
-      fetch ('/emails/sent')
-      .then(response => response.json())
-      .then(emails => {
-          // Print emails
-          console.log(emails);
+      // Load user's SENT mailbox HERE.
+      load_mailbox('sent');
 
-          // Do something else with emails
-      });
-
+      // Prevent default submission of form
       return false;
 
     }
@@ -68,4 +61,31 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Load emails using API
+
+  fetch (`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+      // Print emails
+      console.log(emails);
+
+      // Do something else with emails
+      emails.forEach(add_email_header);
+
+  });
+
+    // Add a new email_header with given contents to DOM
+    function add_email_header(contents)    {
+        // Create new email_header
+        const email_header = document.createElement('div');
+        email_header.className='emailHeader';
+        email_header.innerHTML = contents.sender + contents.subject + contents.timestamp;
+
+        //Add email_header to DOM
+        document.querySelector('#emails-view').append(email_header);
+    }
+
+
+
 }
